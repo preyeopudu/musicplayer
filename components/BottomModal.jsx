7a5398855fd6c9c60fc8209495b505eb21bb3fe9
Opudu Preye <preyeopudu@gmail.com>
2022-05-01 21:43:13 +0100

    close and open modal

--- components/BottomModal.jsx
+++ components/BottomModal.jsx
@@ -1,13 +1,68 @@
 import React, { useEffect, useState } from "react";
-import { Modal, View, Text } from "react-native";
+import {
+  Modal,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { ScaledSheet } from "react-native-size-matters";
 
-const BottomModal = () => {
+const BottomModal = ({ visible, setVisible }) => {
   return (
-    <Modal transparent visible={visible} animationType="slide">
-      <View style={StyleSheet.container}>
-        <Text>music_title.mp3</Text>
-      </View>
-    </Modal>
+    <View>
+      <StatusBar hidden />
+      <Modal transparent visible={visible} animationType="slide">
+        <TouchableWithoutFeedback onPress={() => setVisible(false)}>
+          <View style={styles.container}>
+            <View style={styles.modalContainer}>
+              <Text style={styles.title}>music_title.mp3</Text>
+              <TouchableOpacity
+                activeOpacity={0.8}
+                style={styles.buttonContainer}
+              >
+                <Text style={styles.buttonText}>Play</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                activeOpacity={0.8}
+                style={styles.buttonContainer}
+              >
+                <Text style={styles.buttonText}>Add to Playlist</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+    </View>
   );
 };
+
+const styles = ScaledSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(1,1,1,0.2)",
+  },
+  modalContainer: {
+    bottom: 0,
+    position: "absolute",
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    flex: 1,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    width: "100%",
+  },
+  title: {
+    marginVertical: 10,
+    fontSize: "18@s",
+    color: "#686868",
+  },
+  buttonText: {
+    fontSize: "18@s",
+    marginVertical: 10,
+  },
+});
+
+export default BottomModal;
--- screens/bottomTab/AudioListScreen.jsx
+++ screens/bottomTab/AudioListScreen.jsx
@@ -1,60 +1,81 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, Text, View, Image, TouchableOpacity } from "react-native";
+import {
+  FlatList,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Alert,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { ScaledSheet } from "react-native-size-matters";
 import { useSelector } from "react-redux";
 import ConvertTime from "../../utility/ConvertTime";
 import { Audio } from "expo-av";
+import { Entypo } from "@expo/vector-icons";
+import BottomModal from "../../components/BottomModal";
 
 export default function AudioListScreen() {
+  const [visible, setVisible] = useState(false);
   const reduxData = useSelector((s) => s);
   const musicList = reduxData.music;
 
+  const openModal = () => {
+    setVisible(true);
+  };
+
   const PlayAudio = async (audio) => {
-    const { sound: playbackObject } = await Audio.Sound.createAsync(
-      { uri: audio },
-      { shouldPlay: true }
-    );
+    // const { sound: playbackObject } = await Audio.Sound.createAsync(
+    //   { uri: audio },
+    //   { shouldPlay: true }
+    // );
   };
 
   return (
-    <FlatList
-      style={{ backgroundColor: "#fff" }}
-      data={musicList}
-      keyExtractor={(item) => item.id}
-      renderItem={({ item }) => (
-        <TouchableOpacity
-          activeOpacity={0.7}
-          onPress={() => {
-            PlayAudio(item.uri);
-          }}
-          style={{
-            backgroundColor: "#fff",
-            elevation: 10,
-            marginVertical: 5,
-            borderRadius: 10,
-            paddingVertical: 10,
-            marginHorizontal: 10,
-          }}
-        >
-          <View style={styles.audioContainer}>
-            <View style={styles.imageContainer}>
-              <Image
-                source={require("../../assets/images/music.png")}
-                style={{ width: 25, height: 25 }}
-              />
+    <View>
+      <FlatList
+        style={{ backgroundColor: "#fff" }}
+        data={musicList}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            activeOpacity={0.9}
+            onPress={() => {
+              PlayAudio(item.uri);
+            }}
+            style={{
+              backgroundColor: "#fff",
+              elevation: 10,
+              marginVertical: 5,
+              borderRadius: 10,
+              paddingVertical: 10,
+              marginHorizontal: 10,
+            }}
+          >
+            <View style={styles.audioContainer}>
+              <View style={styles.imageContainer}>
+                <Image
+                  source={require("../../assets/images/music.png")}
+                  style={{ width: 25, height: 25 }}
+                />
+              </View>
+              <View style={styles.textContainer}>
+                <Text style={styles.audioTitle} numberOfLines={1}>
+                  {item.filename.substring(0, 39)}
+                </Text>
+                <Text style={styles.audioLength}>
+                  {ConvertTime(item.duration / 60)}
+                </Text>
+              </View>
+              <TouchableOpacity onPress={() => openModal()}>
+                <Entypo name="dots-three-vertical" size={20} color="#808080" />
+              </TouchableOpacity>
             </View>
-            <View>
-              <Text style={styles.audioTitle} numberOfLines={1}>
-                {item.filename.substring(0, 39)}
-              </Text>
-            </View>
-          </View>
-          <Text style={styles.audioLength}>
-            {ConvertTime(item.duration / 60)}
-          </Text>
-        </TouchableOpacity>
-      )}
-    />
+          </TouchableOpacity>
+        )}
+      />
+      <BottomModal visible={visible} setVisible={setVisible} />
+    </View>
   );
 }
 
@@ -67,26 +88,28 @@ const styles = ScaledSheet.create({
   },
   audioContainer: {
     flexDirection: "row",
-    marginHorizontal: 5,
+    marginHorizontal: 2,
     paddingHorizontal: 10,
     alignItems: "center",
+    justifyContent: "space-between",
   },
   audioStart: {
     padding: 30,
   },
   audioTitle: {
     marginVertical: 5,
-    width: "240@s",
   },
   imageContainer: {
     borderRadius: 100,
     backgroundColor: "#000",
     padding: 5,
-    marginHorizontal: 10,
+    marginRight: "15@s",
   },
   audioLength: {
     fontSize: 10,
-    textAlign: "right",
-    right: 15,
+    color: "#808080",
+  },
+  textContainer: {
+    width: "220@s",
   },
 });
